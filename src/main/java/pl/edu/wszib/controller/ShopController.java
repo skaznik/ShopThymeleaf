76551b3aa70b0d321d1b96bbe1c3f06a965fe346
skaznik <skaznik@wszib.edu.pl>
2020-02-29 11:24:33 +0100

    semestr2_thymeleaf

--- src/main/java/pl/edu/wszib/controller/ShopController.java
+++ src/main/java/pl/edu/wszib/controller/ShopController.java
@@ -1,6 +1,7 @@
 package pl.edu.wszib.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
@@ -11,7 +12,13 @@ public class ShopController {
         return "welcome";
     }
     @GetMapping("shipping")
-    public String shipping() {
+    public String shipping(Model model) {
+        model.addAttribute("shippingMethodName1", "Paczkomat");
+        model.addAttribute("shippingMethodName2", "Kurier - przedpłata");
+        model.addAttribute("shippingMethodName3", "Kurier - pobranie");
+        model.addAttribute("shippingMethodPrice1", "10.50");
+        model.addAttribute("shippingMethodPrice2", "15.00");
+        model.addAttribute("shippingMethodPrice3", "20.20");
         return "shipping";
     }
 }
