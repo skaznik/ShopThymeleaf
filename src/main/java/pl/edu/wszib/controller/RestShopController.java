6b3791a8c9975bbb00542416ab49a655566e0c4c
AdamSkaznik <46684800+AdamSkaznik@users.noreply.github.com>
2020-03-03 20:31:10 +0100

    home

---
+++ src/main/java/pl/edu/wszib/controller/RestShopController.java
@@ -0,0 +1,26 @@
+package pl.edu.wszib.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+import pl.edu.wszib.dao.ProductDao;
+import pl.edu.wszib.domain.Product;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("api")
+@CrossOrigin
+public class RestShopController {
+
+    @Autowired
+    private ProductDao productDao;
+
+    @GetMapping("products")
+    public List<Product> products(){
+        return productDao.getProducts();
+    }
+    @DeleteMapping("products/remove/{id}")
+    public void removeProduct(@PathVariable Long id){
+        productDao.removeProduct(id);
+    }
+}
